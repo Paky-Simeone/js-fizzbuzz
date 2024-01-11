1812fb7c73bf35c82ae31866bfb6799da45f1446
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-11 18:30:55 +0100

    bonus2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 const riga = document.getElementById("riga");
 
+
 for(let i=1; i<=100; i++ ){
+    let numeri;
     if(i % 15==0){
+        numeri = "FizzBuzz";
+        const card = `<div class="card m-2 p-3 bg-danger">${numeri}</div>`;
+        riga.innerHTML += card;
         console.log("FizzBuzz");
     }
     else if(i % 5==0){
+        numeri = "Buzz";
+        const card = `<div class="card m-2 p-3 bg-warning">${numeri}</div>`;
+        riga.innerHTML += card;
         console.log("Buzz");
     }
     else if(i % 3==0){
+        numeri = "Fizz";
+        const card = `<div class="card m-2 p-3 bg-info">${numeri}</div>`;
+        riga.innerHTML += card;
         console.log("Fizz");
     }
     else{
+        numeri = i;
+        const card = `<div class="card m-2 p-3 bg-dark text-white">${numeri}</div>`;
+        riga.innerHTML += card;
         console.log(i);
     }
+    // const card = `<div class="card m-3">${numeri}</div>`;
+    // riga.innerHTML += card;
 
 }
